ece0cac2cd19bce29db0038a52384ebc76d0efca
bakawoot <agodaniel1@gmail.com>
2023-05-31 06:07:42 +0200

    Update SeaBlocks.kt
    Rename some stuff.

--- src/seadustry/content/SeaBlocks.kt
+++ src/seadustry/content/SeaBlocks.kt
@@ -4,14 +4,14 @@ import mindustry.content.Items
 import mindustry.type.Category
 import mindustry.type.ItemStack
 import mindustry.world.Block
-import seadustry.world.Landfill
+import seadustry.world.CreateLand
 
 class SeaBlocks {
     companion object {
-        lateinit var landfill: Block
+        lateinit var createLand: Block
 
         fun load() {
-            landfill = Landfill("landfill").apply {
+            createLand = CreateLand("createLand").apply {
                 requirements(Category.effect, ItemStack.with(Items.sand, 24))
                 alwaysUnlocked = true
             }
