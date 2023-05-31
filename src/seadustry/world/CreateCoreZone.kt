9166f99e6efae2ba725be280c415024f304c95c5
bakawoot <agodaniel1@gmail.com>
2023-05-31 12:19:05 +0200

    Add files via upload

---
+++ src/seadustry/world/CreateCoreZone.kt
@@ -0,0 +1,30 @@
+package seadustry.world
+
+import mindustry.Vars.renderer
+import mindustry.content.Blocks
+import mindustry.game.Team
+import mindustry.gen.Building
+import mindustry.world.Block
+import mindustry.world.Tile
+import mindustry.world.blocks.environment.Floor
+
+open class CreateCoreZone(name: String) : Block(name) {
+
+    init {
+        breakable = true
+        floating = true
+        update = true
+    }
+
+    override fun canPlaceOn(tile: Tile?, team: Team?, rotation: Int): Boolean {
+        return tile != null && tile.floor().liquidDrop == null
+    }
+
+    inner class CreateCoreZoneBuild : Building() {
+        override fun updateTile() {
+            tile.setFloor(Blocks.coreZone as Floor)
+            tile.setAir()
+            renderer.blocks.floor.recacheTile(tile)
+        }
+    }
+}
