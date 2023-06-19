c001107a7561c625457ff339091346b14cb4921b
bakawoot <agodaniel1@gmail.com>
2023-06-19 06:14:40 +0200

    Update CreateLand.kt
    Finally, mapgen.

--- src/seadustry/world/CreateLand.kt
+++ src/seadustry/world/CreateLand.kt
@@ -1,12 +1,15 @@
 package seadustry.world
 
 import mindustry.Vars.renderer
+import mindustry.Vars
+import mindustry.gen.Call
 import mindustry.content.Blocks
 import mindustry.game.Team
 import mindustry.gen.Building
 import mindustry.world.Block
 import mindustry.world.Tile
 import mindustry.world.blocks.environment.Floor
+import arc.util.noise.*
 
 open class CreateLand(name: String) : Block(name) {
 
@@ -22,7 +25,28 @@ open class CreateLand(name: String) : Block(name) {
 
     inner class CreateLandBuild : Building() {
         override fun updateTile() {
-            tile.setFloor(Blocks.sand as Floor)
+            Noise.setSeed(50)
+            val tileNoise: Float = Noise.noise(tile.x.toFloat(), tile.y.toFloat(), 40f, 50f)
+            
+            if (tileNoise <= -11)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.ice as Floor)
+            if (tileNoise >= -11 && tileNoise < -9)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.snow as Floor)
+            if (tileNoise >= -9 && tileNoise < -6)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.moss as Floor)
+            if (tileNoise >= -6 && tileNoise < -3)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.grass as Floor)
+            if (tileNoise >= -3 && tileNoise < 0)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.sand as Floor)
+            if (tileNoise >= 0 && tileNoise < 3)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.darksand as Floor)
+            if (tileNoise >= 3 && tileNoise < 6)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.stone as Floor)
+            if (tileNoise >= 6 && tileNoise < 9)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.carbonStone as Floor)
+            if (tileNoise >= 9)
+                Vars.world.tile(tile.x.toInt(),tile.y.toInt()).setFloor(Blocks.basalt as Floor)
+            
             tile.setAir()
             renderer.blocks.floor.recacheTile(tile)
         }
